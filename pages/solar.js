dcbdb6d4e536fd68d084ba20459b9a9940498674
YanikaD <yanika.dontong@gmail.com>
2023-10-09 09:55:51 +0700

    add script

--- pages/solar.js
+++ pages/solar.js
@@ -1,7 +1,7 @@
 // import "./solar.css";
-// import * as THREE from "/build/three.module.js";
-// import { OrbitControls } from "../../js/OrbitControls";
-// import * as TWEEN from "/src/Tween.js";
+import * as THREE from "../three/build/three.module";
+import { OrbitControls } from "../three/examples/jsm/controls/OrbitControls.js";
+import * as TWEEN from "../tween.js/src/Tween.js";
 
 const scene = new THREE.Scene();
 const camera = getCamera();
